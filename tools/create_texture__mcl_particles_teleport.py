66607c3e37b30fb580bfd3bf248822783c7d3030
kay27 <kay27@bk.ru>
2020-08-30 18:39:31 +0400

    Update Nether portal texture

---
+++ tools/create_texture__mcl_particles_teleport.py
@@ -0,0 +1,20 @@
+import png
+
+s = [
+	'0000010',
+	'0101100',
+	'0010111',
+	'0101010',
+	'1010111',
+	'0001100',
+	'0010100',
+]
+
+s = [[int(c) for c in row] for row in s]
+
+palette=[(0x00,0x00,0x00,0x00), (0xcf,0x00,0xcf,0xe0)]
+
+w = png.Writer(len(s[0]), len(s), palette=palette, bitdepth=1)
+f = open('mcl_particles_teleport.png', 'wb')
+w.write(f, s)
+
