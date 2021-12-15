3a402c80548dde969dee55c7a35d9af25252631a
kay27 <kay27@bk.ru>
2021-12-15 04:20:34 +0400

    Fix minor ambient light issues

--- mods/ENVIRONMENT/mcl_ambient_light/README.txt
+++
@@ -1,7 +0,0 @@
-ambient_light by MikeRedwood
-============================
-Makes all nodes lit to a small degree!
-
-License of source code:
------------------------
-GNU LESSER GENERAL PUBLIC LICENSE Version 2.1, February 1999
--- mods/ENVIRONMENT/mcl_ambient_light/init.lua
+++ mods/ENVIRONMENT/mcl_ambient_light/init.lua
@@ -1,6 +1,5 @@
-local light_min = tonumber(minetest.settings:get("light_min")) or 1
-
 minetest.register_on_mods_loaded(function ()
+	local light_min = 1
 	for i, def in pairs(minetest.registered_nodes) do
 		local light_source = def.light_source
 		if light_source == nil or light_source < light_min then
