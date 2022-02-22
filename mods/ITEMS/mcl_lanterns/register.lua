f23fc7c6a1380a348096eaebc61327b9f05facec
PrairieAstronomer <prairieastronomer@noreply.git.minetest.land>
2022-02-22 18:05:22 +0000

    Add 'mods/ITEMS/mcl_lanterns/register.lua'

---
+++ mods/ITEMS/mcl_lanterns/register.lua
@@ -0,0 +1,18 @@
+local S = minetest.get_translator("mcl_lanterns")
+
+mcl_lanterns.register_lantern("lantern", {
+	description = S("Lantern"),
+	longdesc = S("Lanterns are light sources which can be placed on the top or the bottom of most blocks."),
+	texture = "mcl_lanterns_lantern.png",
+	texture_inv = "mcl_lanterns_lantern_inv.png",
+	light_level = 14,
+})
+
+minetest.register_craft({
+	output = "mcl_lanterns:lantern_floor",
+	recipe = {
+		{"mcl_core:iron_nugget", "mcl_core:iron_nugget", "mcl_core:iron_nugget"},
+		{"mcl_core:iron_nugget", "mcl_torches:torch"   , "mcl_core:iron_nugget"},
+		{"mcl_core:iron_nugget", "mcl_core:iron_nugget", "mcl_core:iron_nugget"},
+	},
+})
