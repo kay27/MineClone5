e61e06542ef4a40c3e43fbfd45b4b90c7d51f885
PrairieAstronomer <prairieastronomer@noreply.git.minetest.land>
2022-02-22 22:30:00 +0000

    Update 'mods/ITEMS/mcl_lanterns/register.lua'

--- mods/ITEMS/mcl_lanterns/register.lua
+++ mods/ITEMS/mcl_lanterns/register.lua
@@ -16,3 +16,20 @@ minetest.register_craft({
 		{"mcl_core:iron_nugget", "mcl_core:iron_nugget", "mcl_core:iron_nugget"},
 	},
 })
+
+mcl_lanterns.register_lantern("soul_lantern", {
+	description = S("Soul Lantern"),
+	longdesc = S("Lanterns are light sources which can be placed on the top or the bottom of most blocks."),
+	texture = "mcl_lanterns_soul_lantern.png",
+	texture_inv = "mcl_lanterns_soul_lantern_inv.png",
+	light_level = 10,
+})
+
+minetest.register_craft({
+	output = "mcl_lanterns:soul_lantern_floor",
+	recipe = {
+		{"mcl_core:iron_nugget", "mcl_core:iron_nugget", "mcl_core:iron_nugget"},
+		{"mcl_core:iron_nugget", "mcl_blackstone:soul_torch", "mcl_core:iron_nugget"},
+		{"mcl_core:iron_nugget", "mcl_core:iron_nugget", "mcl_core:iron_nugget"},
+	},
+})
