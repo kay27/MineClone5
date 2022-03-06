926382b6dcb0cf7515d256b9cec6b28bdb945be6
3raven <elise_declerck@laposte.net>
2022-03-06 23:26:41 +0000

    Transférer les fichiers vers 'mods/ITEMS/mclx_doors'

---
+++ mods/ITEMS/mclx_doors/init.lua
@@ -0,0 +1,9 @@
+mclx_doors = {}
+
+local this = minetest.get_current_modname()
+local path = minetest.get_modpath(this)
+
+dofile(path.."/api_doors.lua") -- Doors API
+dofile(path.."/api_trapdoors.lua") -- Trapdoors API
+dofile(path.."/register.lua") -- Register builtin doors and trapdoors
+dofile(path.."/alias.lua") -- Legacy aliases
