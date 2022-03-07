0e00644e73a16be3eb1d43af8075b8d8ad9f12ae
3raven <elise_declerck@laposte.net>
2022-03-07 01:37:41 +0000

    Supprimer 'mods/ITEMS/mclx_doors/init.lua'

--- mods/ITEMS/mclx_doors/init.lua
+++
@@ -1,9 +0,0 @@
-mclx_doors = {}
-
-local this = minetest.get_current_modname()
-local path = minetest.get_modpath(this)
-
-dofile(path.."/api_doors.lua") -- Doors API
-dofile(path.."/api_trapdoors.lua") -- Trapdoors API
-dofile(path.."/register.lua") -- Register builtin doors and trapdoors
-dofile(path.."/alias.lua") -- Legacy aliases
