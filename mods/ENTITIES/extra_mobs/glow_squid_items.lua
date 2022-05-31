6df9027971b9bdc71041b497d6027f50034da68f
3raven <elise_declerck@laposte.net>
2022-05-31 12:54:31 +0000

    Fleckenstein's fix for mcl2

--- mods/ENTITIES/extra_mobs/glow_squid_items.lua
+++ mods/ENTITIES/extra_mobs/glow_squid_items.lua
@@ -86,7 +86,10 @@ minetest.register_entity("extra_mobs:glow_item_frame_map", {
 	},
 	on_activate = function(self, staticdata)
 		self.id = staticdata
-		self.object:set_properties({textures = {mcl_maps.load_map(self.id)}})
+		mcl_maps.load_map(self.id, function(texture)
+			-- will not crash even if self.object is invalid by now
+			self.object:set_properties({textures = {texture}})
+		end)
 	end,
 	get_staticdata = function(self)
 		return self.id
